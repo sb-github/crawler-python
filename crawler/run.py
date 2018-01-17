69f22b328a9e9c29ee54e98fa5fd393cefce6db0
Eduard Kovalchuk <edward.kovlachuk11@gmail.com>
2018-01-17 16:05:55 +0200

    Changed Crawlers run and setup methods in crawler module; run module added

--- crawler/run.py
+++ crawler/run.py
@@ -1,4 +1,5 @@
-'''import sys
+import sys
+
 from crawler import Crawler
 
 c = Crawler(_id=sys.argv[1])
@@ -6,16 +7,3 @@ c = Crawler(_id=sys.argv[1])
 c.setup()
 
 c.run()
-'''
-
-import sys
-
-print(sys.path)
-
-sys.path.append('..')
-
-print(sys.path)
-
-import pars.parse
-
-print(pars.parser.foo)
