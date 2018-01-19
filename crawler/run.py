14a3482248acc85cb890919cd8951cbba60b26fc
Eduard Kovalchuk <edward.kovlachuk11@gmail.com>
2018-01-19 16:45:21 +0200

    new stuff

--- crawler/run.py
+++ crawler/run.py
@@ -2,7 +2,8 @@ import sys
 
 from crawler import Crawler
 
-c = Crawler(_id=sys.argv[1])
+# c = Crawler(_id=sys.argv[1])
+c = Crawler(_id="5a5f74b13e7d66747f062e2f")
 
 c.setup()
 
--- db_config/mongodb_setup.py
+++ db_config/mongodb_setup.py
@@ -1,12 +1,28 @@
+import yaml, sys
+
 import pymongo
+
+from os.path import dirname
+
 
 
 class Data_base:
+    
     def __init__(self, db_name):
         self.db_name = db_name
 
+
+    def get_mongo_ip(self):
+        project_root = dirname(sys.path[0])
+        yaml_rel_path = "/resources/config.yaml"
+        yaml_path = project_root + yaml_rel_path
+        with open(yaml_path) as yaml_file:
+            conf = yaml.load(yaml_file)
+        mongo_ip = conf['mongo_db']['ip']
+        return mongo_ip
+
+
     def connect_db(self):
-        client = pymongo.MongoClient('192.168.128.231:27017')
+        client = pymongo.MongoClient(self.get_mongo_ip())
         db = client[self.db_name]
         return db
-
