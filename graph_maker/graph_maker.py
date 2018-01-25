20f71c17e440e58b49e2a48da05236c24e657e9f
ruslan-immunity <ruslan.immunity@gmail.com>
2018-01-25 12:14:06 +0200

    the resolution of the conflict

--- graph_maker/graph_maker.py
+++ graph_maker/graph_maker.py
@@ -8,8 +8,8 @@ class Data_base:
 
     """The connection to the database"""
     def connect_db(self):
-        client = pymongo.MongoClient('localhost:27017')
-        db = client['Vacancies']
+        client = pymongo.MongoClient('192.168.128.231:27017')
+        db = client['crawler']
         posts = db[self.collection_name]
         return posts
 
@@ -67,16 +67,17 @@ class Graph_maker:
             # select all skills
             for pars_skill in pars_vac['raw_vacancy']:
                 arr_raw_vacancy.append(pars_skill)
-
+            # check vacancy on сrawler_id
             if data_graph_skill.find({'crawler_id': pars_vac['crawler_id']}).count() != 0:
                 arr_graph_skill.clear()
                 arr_graph_connects.clear()
-
+                # if crawler_id has, then we read data there is in graph_skill
                 for graph_skill in data_graph_skill.find({'crawler_id': pars_vac['crawler_id']}):
                     arr_graph_skill.append(graph_skill['skill'])
                     arr_graph_connects.append(graph_skill['connects'])
-
+                # take one word from raw_vacancy
                 for i in range(len(arr_raw_vacancy)):
+                    # check whether the word skill
                     if arr_graph_skill.count(arr_raw_vacancy[i]) == 1:
                         connects = arr_graph_connects[arr_graph_skill.index(arr_raw_vacancy[i])]
                         arr_sub_skill.clear()
@@ -89,10 +90,13 @@ class Graph_maker:
                             arr_weight.append(connect['weight'])
 
                         con.clear()
+
                         for s in arr_graph_connects[arr_graph_skill.index(arr_raw_vacancy[i])]:
                             con.append(s)
-
+                        """if the word is skill, then word goes through a cycle with all words, but word,
+                        which is a skill """
                         for a in range(i):
+                            # The scan starts sub_skills, if word == sub_skill, then start his updating
                             if arr_sub_skill.count(arr_raw_vacancy[a]) >= 1:
                                 parser_id.clear()
                                 sub_skill = {'subskill': arr_sub_skill[arr_sub_skill.index(arr_raw_vacancy[a])]}
@@ -107,6 +111,7 @@ class Graph_maker:
 
                                 self.delete_repeat_connect(con, sub_skill)
                                 con.append(sub_skill)
+                            # if word != sub_skill, then word added as a skill
                             else:
                                 self.insert_one_subskill(parser_id1, pars_vac, arr_raw_vacancy, a, con)
 
@@ -130,14 +135,15 @@ class Graph_maker:
                         data_graph_skill.update({'crawler_id': pars_vac['crawler_id'], 'skill': arr_raw_vacancy[i]},
                                                 {'crawler_id': pars_vac['crawler_id'], 'skill': arr_raw_vacancy[i],
                                                  'connects': con})
-
+                    # if word is not skill, then creates a new skill with their sub_skills
                     else:
                         self.insert_one_skill(arr_raw_vacancy, con, parser_id, data_graph_skill, i, pars_vac)
+                    print(arr_raw_vacancy[i])
+            # if no сrawler_id, then creates skills with their sub_skills
             else:
                 for i in range(len(arr_raw_vacancy)):
                     self.insert_one_skill(arr_raw_vacancy, con, parser_id, data_graph_skill, i, pars_vac)
         for pars_vac in data_vacancy.find({'status': 'IN_PROCESS'}):
-            arr_raw_vacancy.clear()
             self.change_status('parsed_vacancy', pars_vac)
 
     def change_status(self, name_database, data_vacancy):
@@ -158,3 +164,5 @@ class Graph_maker:
     def run(self):
         self.graph_maker()
 
+# maker = Graph_maker()
+# maker.run()
