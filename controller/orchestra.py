00b43fa84e7448c9350bec0c754bd2a18206c6fd
balloonka135 <balloonka135@gmail.com>
2018-01-26 13:19:38 +0200

    added jenkins

---
+++ controller/orchestra.py
@@ -0,0 +1,15 @@
+class Orchestra:
+
+    def __init__(self, controller):
+        self.controller = controller
+
+    def start_rest(self):
+        pass
+
+    def start_cronjob(self):
+        pass
+
+    def get_all_procs(self):
+        pass
+
+    
