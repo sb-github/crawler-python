4c3976ea4d745c9bbc3814bee103cd513254951d
balloonka135 <balloonka135@gmail.com>
2018-01-31 16:04:36 +0200

    testing cronjob

--- parser/run.py
+++ parser/run.py
@@ -3,7 +3,5 @@ from parser import Parser_vacancy
 import datetime
 
 
-# p = Parser_vacancy()
-# p.run()
-with open('info.txt', 'a') as outFile:
-    outFile.write('\n' + str(datetime.datetime.now()))
+p = Parser_vacancy()
+p.run()
